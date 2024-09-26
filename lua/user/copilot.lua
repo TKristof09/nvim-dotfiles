ed7624eb9f9f2bde3dcad0f65ed4c7883d14bbec
Kristof Toth <toth.kristof111@gmail.com>
2024-09-26 11:12:08 +0200

    Make copilot on demand instead of constantly active

--- lua/user/copilot.lua
+++ lua/user/copilot.lua
@@ -1,11 +1,27 @@
 
 require("copilot").setup{
-    suggestion =
-    {
-        auto_trigger = true,
-        keymap = {
-            next = "<M-J>",
-            prev = "<M-K>"
-        }
-    },
+    panel = {
+        auto_refresh =  true,
+        keymap =
+            {
+                open = false,
+                jump_next = "<C-D>",
+                jump_prev = "<C-U>",
+                refresh = "<leader>cr",
+            },
+    }
 }
+
+-- keymap for toggling the panel
+local win =  -1
+vim.keymap.set('n', '<leader>co', function()
+    if not vim.api.nvim_win_is_valid(win) then
+        require("copilot.panel").open()
+        win = vim.api.nvim_get_current_win()
+    else
+        vim.api.nvim_win_close(win, false)
+        win = -1
+    end
+end,
+    {noremap = true, silent = true}
+)
